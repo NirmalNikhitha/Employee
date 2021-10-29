692c58ffc787964d5af1a2b2fda03b57b5ef7976
NirmalNikhitha <92016228+NirmalNikhitha@users.noreply.github.com>
2021-10-29 19:52:13 +0530

    Employee
    This program executes the detail of the employee

---
+++ Employee.java
@@ -0,0 +1,10 @@
+class information
+{
+  public static void main(String args[])
+  {
+    System.out.println("Name year of joining adddress");
+System.out.println("Robert 1994 64C-WallsStreat");
+System.out.println("Sam 2000 68D-WallsStreat");
+System.out.println("John 1999 26B-WallsStreat");
+}
+}
